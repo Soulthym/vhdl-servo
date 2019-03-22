94b488b80cfac3e7a64e6cfa42e089ca7d916cad
Soulthym <thymi92@gmail.com>
2019-03-22 19:10:47 +0100

    mcode and llvm compatibility mode

--- test_control_servo.sh
+++ test_control_servo.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 project="servo"
+
 ghdl --clean
 
-ghdl -a $project.vhd 
-ghdl -a tb_$project.vhd 
+case $1 in 
+    llvm)
+        ghdl -a $project.vhd 
+        ghdl -a tb_$project.vhd 
 
-ghdl -c -e tb_$project
-ghdl -c -r tb_$project --wave=tb_$project.ghw
+        ghdl -c -e tb_$project
+        ghdl -c -r tb_$project --wave=tb_$project.ghw
 
-gtkwave tb_$project.ghw
+        gtkwave tb_$project.ghw
+        ;;
+    mcode)
+        ghdl -a $project.vhd
+        ghdl -e $project
+
+        ghdl -a tb_$project.vhd
+        ghdl -e tb_$project
+        ghdl -r tb_$project --wave=tb_$project.ghw
+
+        gtkwave tb_$project.ghw
+        ;;
+esac
 
 rm tb_$project.ghw work-obj93.cf
