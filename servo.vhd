875e948408b4349ddc2256a83bf5ac8811b34298
Soulthym <thymi92@gmail.com>
2019-03-25 15:24:23 +0100

    new file

--- servo.vhd
+++ servo.vhd
@@ -1,84 +1,53 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.Numeric_std.all;
-                     
 
 entity SERVO is
-	generic ( bit_count : integer := 255 );	--- divide sys clock into 1/255 ms clock
-	port (	Clk : in STD_LOGIC;
-		reset: in STD_LOGIC;
-		position : in STD_lOGIC_VECTOR(7 downto 0);
-		gpio_clk_out : out STD_LOGIC
-	);
+    generic ( clkcnt : integer := 255 );    --- divide sys clock into 1/255 ms clock
+    port (    
+        clk : in STD_LOGIC;
+        rst: in STD_LOGIC;
+        pos : in STD_lOGIC_VECTOR(7 downto 0);
+        gpio : out STD_LOGIC
+    );
 end entity;
 
 architecture SERVO_CONTROL of SERVO is
-signal count : std_logic_vector(7 downto 0); ---1 ms count---
-signal total : integer range 0 to (18*(bit_count-1)):= 0;
+signal cnt : integer range 0 to 5119;
 signal servo_clk : std_logic;
-signal servo_count : INTEGER range 0 to bit_count-1 := 0;
-
-type state_machine_counter is (Idle, Unit, Added);
-signal state_counter : state_machine_counter;
+signal servo_cnt : INTEGER range 0 to clkcnt := 0;
 
 begin
 
-process(Clk, reset)
-begin
---- clock divider-----
-	if reset = '1' then
-		servo_clk <= '0';
-		servo_count <= 0;
-	elsif rising_edge(Clk) then	--- generate servo clock ---
-		if servo_count < bit_count-1 then	
-			servo_count <= servo_count + 1;
-			servo_clk <= '0';
-		else
-			servo_count <= 0;
-			servo_clk <= '1';
-		end if;
-	end if;
-end process;
-
-process(Clk, reset, servo_clk)
-begin
---- servo signal generator ----
-	if reset = '1' then
-		state_counter <= Idle;
-		gpio_clk_out <= '0';
-		count <= "00000000";
-		total <= 0;
-	elsif rising_edge(Clk) then
-		if servo_clk = '1' then
-			case state_counter is
-
-			when Idle =>	---- idle mode : no signal ---
-				if total < (18*(bit_count-1)) then
-					total <= total+1;
-				else
-					total <= 0;
-					state_counter <= Unit;
-				end if;
-				gpio_clk_out <= '0';
-			when Unit =>	---- 1ms pulse ----
-				if count = "11111111" then
-					count <= "00000000";
-					state_counter <= Added;
-				else
-					count <= std_logic_vector(unsigned(count) +1);
-				end if;
-				gpio_clk_out <= '1';
-			when Added =>	---- servo angle control pulse ----
-				if count = position then
-					count <= "00000000";
-					state_counter <= Idle;
-				else
-					count <=  std_logic_vector(unsigned(count) +1);
-				end if;
-				gpio_clk_out <= '1';
-			end case;
-		end if;
-	end if;
-end process;
+    process(clk, rst)
+    begin
+    --- servo signal generato ----
+        if rst = '1' then
+            gpio <= '0';
+            cnt <= 0;
+            servo_clk <= '0';
+        end if;
+        if rising_edge(clk) then
+            if servo_cnt < clkcnt then
+                servo_clk <= '0';
+                servo_cnt <= servo_cnt + 1;
+            else
+                servo_clk <= '1';
+                servo_cnt <= 0;
+                if cnt < 5119 then
+                    cnt <= cnt + 1;
+                else
+                    cnt <= 0;
+                end if;
+            end if;
+            if servo_clk = '1' then
+                if cnt < (255 + to_integer(Unsigned(pos))) then
+                    gpio <= '1';
+                else
+                    gpio <= '0';
+                end if;
+            end if;
+        end if;
+    end process;
 
 end architecture;
--- tb_servo.vhd
+++ tb_servo.vhd
@@ -1,17 +1,16 @@
 -- Banc de test pourl'exercice sur les machines à état
 
 entity TB_SERVO is
-	port( OK: out BOOLEAN := TRUE);
+    port( OK: out BOOLEAN := TRUE);
 end entity;
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 architecture A1 of TB_SERVO is
-	signal clk, rst: STD_LOGIC := '0';
-	signal pos : std_logic_vector(7 downto 0);
-	signal gpio : STD_LOGIC;
-
+    signal clk, rst: STD_LOGIC := '0';
+    signal pos : std_logic_vector(7 downto 0);
+    signal gpio : STD_LOGIC;
 begin
 
   process
@@ -27,27 +26,27 @@ begin
 
   process
    begin
- 	rst <= '1';
-	wait for 10 mS;
+     rst <= '1';
+    wait for 10 mS;
 
-	rst <= '0';
-	pos <= "00000000";
-	wait for 10 mS;
-	
-	pos <= "00001111";
-	wait for 10 mS;
-	
-	pos <= "11111111";
-	wait for 10 mS;
-	wait;
+    rst <= '0';
+    pos <= "00000000";
+    wait for 10 mS;
+    
+    pos <= "00001111";
+    wait for 10 mS;
+    
+    pos <= "11111111";
+    wait for 10 mS;
+    wait;
   
   end process;
   UUT: entity work.SERVO(servo_control)
-  	generic map ( bit_count => 255 )
-  	port map (	
-  			Clk => clk,
-			reset => rst, 
-			position => pos,
-			gpio_clk_out => gpio
-		) ;
+      generic map ( clkcnt => 2 )
+      port map (    
+              Clk => clk,
+            rst => rst, 
+            pos => pos,
+            gpio => gpio
+        ) ;
 end;
