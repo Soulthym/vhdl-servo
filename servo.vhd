b2d9c062b7835912aa609ea28950e92c83e91f5a
BaptisteHudyma <bapt.hudyma95650@gmail.com>
2019-03-25 15:09:00 +0100

    Working servo control

--- servo.vhd
+++ servo.vhd
@@ -14,9 +14,9 @@ end entity;
 
 architecture SERVO_CONTROL of SERVO is
 signal count : std_logic_vector(7 downto 0); ---1 ms count---
-signal total : integer range 0 to 4590 := 0;
+signal total : integer range 0 to (18*(bit_count-1)):= 0;
 signal servo_clk : std_logic;
-signal servo_count : INTEGER range 0 to bit_count := 0;
+signal servo_count : INTEGER range 0 to bit_count-1 := 0;
 
 type state_machine_counter is (Idle, Unit, Added);
 signal state_counter : state_machine_counter;
@@ -27,36 +27,38 @@ process(Clk, reset)
 begin
 --- clock divider-----
 	if reset = '1' then
-		state_counter <= Idle;
-		gpio_clk_out <= '0';
-		count <= "00000000";
-		total <= 0;
 		servo_clk <= '0';
+		servo_count <= 0;
 	elsif rising_edge(Clk) then	--- generate servo clock ---
-		if servo_count = bit_count then
-			servo_count <= 0;
-			servo_clk <= not servo_clk;
-		else
+		if servo_count < bit_count-1 then	
 			servo_count <= servo_count + 1;
+			servo_clk <= '0';
+		else
+			servo_count <= 0;
+			servo_clk <= '1';
 		end if;
 	end if;
 end process;
 
 process(Clk, reset, servo_clk)
 begin
---- servo signal generato ----
-	if rising_edge(Clk) then
+--- servo signal generator ----
+	if reset = '1' then
+		state_counter <= Idle;
+		gpio_clk_out <= '0';
+		count <= "00000000";
+		total <= 0;
+	elsif rising_edge(Clk) then
 		if servo_clk = '1' then
 			case state_counter is
 
 			when Idle =>	---- idle mode : no signal ---
-				if total = 4590 then
+				if total < (18*(bit_count-1)) then
+					total <= total+1;
+				else
 					total <= 0;
 					state_counter <= Unit;
-				else
-					total <= total+1;
 				end if;
-
 				gpio_clk_out <= '0';
 			when Unit =>	---- 1ms pulse ----
 				if count = "11111111" then
